f2046445ec29e9864294bbf3f88c7763776d0a41
Anthony Vanoostendorp <anthony.vanoo@live.com>
2018-03-17 10:20:31 +0200

    Fixes

--- breakout/js/gamestart.js
+++ breakout/js/gamestart.js
@@ -7,6 +7,7 @@ let message = document.getElementById("status_line");
 let ballMoving = true;
 let time= Date.now();
 let keysDown = {};
+let amountToBreak = 23;
 let lives = 3;
  
 window.addEventListener("keydown",function(e){
@@ -20,9 +21,9 @@ window.addEventListener("keyup",function(e){
 });
 
 class brick {
-    constructor (xVal,y) {
-        this.xVal= 0;
-        this.y = 59;
+    constructor (xVal,yVal) {
+        this.xVal = xVal;
+        this.yVal = yVal;
         this.width = 80;
         this.height = 15;
         this.visibility = true;
@@ -36,14 +37,41 @@ class brick {
         }
     }
 }
-let brickArray = new Array();
-brickArray[0] = new brick(88,20);
-brickArray[1] = new brick(80,20);
-brickArray[2] = new brick(160,20);
-brickArray[3] = new brick(240,20);
-brickArray[4] = new brick(320,20);
-brickArray[5] = new brick(400,20);
 
+
+let brickArray = [];
+let brickArray2 = [];
+let brickArray3 = [];
+/*
+* WHY YOU NO WORK????
+* FIXED!! made the xVal and yVal = to the same???(xVal = xval) Why did it need to be that?
+*/
+brickArray[0] = new brick(0,50);
+brickArray[1] = new brick(80,50);
+brickArray[2] = new brick(160,50);
+brickArray[3] = new brick(240,50);
+brickArray[4] = new brick(320,50);
+brickArray[5] = new brick(400,50);
+brickArray[6] = new brick(480,50);
+brickArray[7] = new brick(560,50);
+// check the values console.log(brickArray);
+brickArray2[0] = new brick(40,35);
+brickArray2[1] = new brick(120,35);
+brickArray2[2] = new brick(200,35);
+brickArray2[3] = new brick(280,35);
+brickArray2[4] = new brick(360,35);
+brickArray2[5] = new brick(440,35);
+brickArray2[6] = new brick(520,35);
+brickArray2[7] = new brick(800,35);
+
+brickArray3[0] = new brick(0,20);
+brickArray3[1] = new brick(80,20);
+brickArray3[2] = new brick(160,20);
+brickArray3[3] = new brick(240,20);
+brickArray3[4] = new brick(320,20);
+brickArray3[5] = new brick(400,20);
+brickArray3[6] = new brick(480,20);
+brickArray3[7] = new brick(560,20);
 
 // random color gen
 let brickMax = brickArray.length;
@@ -53,6 +81,8 @@ colorGen();
 function colorGen() {
     while (currentBrick < brickMax ) {
         brickArray[currentBrick].color = "#" +  brickArray[currentBrick].randomColor(MAXRGB);
+        brickArray2[currentBrick].color = "#" +  brickArray2[currentBrick].randomColor(MAXRGB);
+        brickArray3[currentBrick].color = "#" +  brickArray3[currentBrick].randomColor(MAXRGB);
         currentBrick++;  
     }
 }
@@ -129,11 +159,67 @@ multiply ySpeed by -1*/
 		// check if ball outside bottom of game area
         if (ballSprite.y > canvasElem.height){
             lives--;
+            // set ballMoving = false if ball outside the game area
             ballMoving = false;
-            message.textContent = "Status: You lost a life! " + lives + " left";
+            if (lives > 0 ){
+                message.textContent = "Status: You lost a life! " + lives + " left";
+                resetBall();
+            } else {
+                message.textContent = "Game over";
+            }
+            
         }
-	    // set ballMoving = false if ball outside the game area
-	}
+	    
+    }
+    /*
+    * need a way to check each bricks visibility level
+    * if one has it false then it need to disappear
+    */
+    for (let i = 0; i < brickArray.length; i++) {
+        
+        if (brickArray[i].visibility) {
+                    //15 + 200 < 40 & x >= 0
+            if ((ballSprite.y < brickArray[i].yVal + brickArray[i].height  ) && (ballSprite.x >= brickArray[i].xVal) && (ballSprite.x <= (brickArray[i].xVal + brickArray[i].width))) {
+                ballSprite.ySpeed *= -1;
+                brickArray[i].visibility = false;
+                amountToBreak--;
+            }
+        }
+    }
+    for (let i = 0; i < brickArray2.length; i++) {
+        
+        if (brickArray2[i].visibility) {
+            if ((ballSprite.y < brickArray2[i].yVal + brickArray2[i].height  ) && (ballSprite.x >= brickArray2[i].xVal) && (ballSprite.x <= (brickArray2[i].xVal + brickArray2[i].width))) {
+                ballSprite.ySpeed *= -1;
+                brickArray2[i].visibility = false;
+                amountToBreak--;
+            }
+        }
+    }
+    for (let i = 0; i < brickArray3.length; i++) {
+        
+        if (brickArray3[i].visibility) {
+                    //15 + 200 < 40 & x >= 0
+            if ((ballSprite.y < brickArray3[i].yVal + brickArray3[i].height  ) && (ballSprite.x >= brickArray3[i].xVal) && (ballSprite.x <= (brickArray3[i].xVal + brickArray3[i].width))) {
+                ballSprite.ySpeed *= -1;
+                brickArray3[i].visibility = false;
+                amountToBreak--;
+            }
+        }
+    }
+    if (amountToBreak == 0 ){
+        ballMoving = false;
+        message.textContent = "YOU WIN!!"
+    }
+}
+
+function resetBall() {
+    if ( lives > 0 ){
+        ballSprite.ySpeed *= -1;
+        ballSprite.x = 130;
+        ballSprite.y = 180;
+        ballMoving = true;
+    }
 }
 
 function renderGame()
@@ -150,11 +236,27 @@ function renderGame()
 	ctx.fillRect(ballSprite.x,ballSprite.y,ballSprite.width,ballSprite.height);
     
     //lay down the top bricks
-    ctx.fillStyle = brickArray[0].color;
-    ctx.fillRect(brickArray[0].x,brickArray[0].y,brickArray[0].width,brickArray[0].height);
-    
-    ctx.fillStyle = brickArray[1].color;
-    ctx.fillRect(brickArray[1].x,brickArray[1].y,brickArray[1].width,brickArray[1].height);
+    for (let i = 0; i < brickArray.length; i++) {
+        
+        if (brickArray[i].visibility) {
+            ctx.fillStyle = brickArray[i].color;
+            ctx.fillRect(brickArray[i].xVal,brickArray[i].yVal,brickArray[i].width,brickArray[i].height);
+        }
+    }
+    for (let i = 0; i < brickArray.length; i++) {
+        
+        if (brickArray2[i].visibility) {
+            ctx.fillStyle = brickArray2[i].color;
+            ctx.fillRect(brickArray2[i].xVal,brickArray2[i].yVal,brickArray2[i].width,brickArray2[i].height);
+        }
+    }
+    for (let i = 0; i < brickArray.length; i++) {
+        
+        if (brickArray3[i].visibility) {
+            ctx.fillStyle = brickArray3[i].color;
+            ctx.fillRect(brickArray3[i].xVal,brickArray3[i].yVal,brickArray3[i].width,brickArray3[i].height);
+        }
+    }
     
     
 }
