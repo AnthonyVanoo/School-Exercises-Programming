373cbab2b463405d92ac922aa6670c5a3ce4b576
Anthony Vanoostendorp <anthony.vanoo@live.com>
2018-05-10 23:18:30 +0300

    Tested adding a custom symbol that could rotate

--- Bus app/js/busScriptUITesting.js
+++ Bus app/js/busScriptUITesting.js
@@ -136,15 +136,18 @@ function showCurrentLocation() {
     }
     function showPosition(position) {
         
+        let busIcon = {
+            path: d="M13.352,27.155c0,0.485-0.393,0.879-0.88,0.879c-0.485,0-0.878-0.394-0.878-0.879c0-0.486,0.393-0.88,0.878-0.88 C12.958,26.275,13.352,26.669,13.352,27.155z M26.478,26.02c-0.487,0-0.88,0.394-0.88,0.877c0,0.487,0.394,0.88,0.88,0.88 c0.484,0,0.877-0.393,0.877-0.88C27.354,26.413,26.962,26.02,26.478,26.02z M30.44,24.122c-0.016,0.664-0.15,3.016-0.15,3.016 l-2.189-0.016c0,0,0.191-1.984-1.645-1.984c-1.717,0-1.678,1.935-1.678,1.935l-10.739-0.029c0,0,0.195-1.673-1.68-1.673 c-1.405,0-1.483,1.76-1.483,1.76L9.04,27.094c0,0-0.22-1.856-0.196-2.459c0.039-0.938,0-2.577,0.157-2.968 c0.144-0.359,4.413-0.281,4.413-0.281s-0.352-4.054-0.183-4.223c0.83-0.829,16.487-0.631,17.055-0.065 C30.597,17.41,30.481,22.289,30.44,24.122z M16,18.509c0-0.568-0.432-1.03-1-1.03c-0.57,0-1,0.461-1,1.03v4.637 c0,0.568,0.43,1.03,1,1.03c0.568,0,1-0.461,1-1.03V18.509z M20,18h-2v2h2V18z M23,18h-2v2h2V18z M26,18h-2v2h2V18z M29,18h-1.688v2 H29V18z",
+            rotate: 0,
+        };
+        //busIcon.style.transform = "rotate(90deg)";
+        
         let currentLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-        alert("Made it to showPos" + currentLocation);
+        //alert("Made it to showPos" + currentLocation);
         let myMarker = new google.maps.Marker({
             position: currentLocation,
             map:myMap,
-            icon: {
-                url: "img/solaireMarkerSmNb.png",
-                rotation: 180,
-            },
+            icon: busIcon,
             title: "Current Location",
         });
         locationMarkers.push(myMarker);
