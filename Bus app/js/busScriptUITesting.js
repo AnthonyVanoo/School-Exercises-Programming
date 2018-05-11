69309e7eeb52145bf6bacef2c955a336e07515c3
Anthony Vanoostendorp <anthony.vanoo@live.com>
2018-05-12 01:02:04 +0300

    Added more custom markers, cleaned up code

--- Bus app/js/busScriptUITesting.js
+++ Bus app/js/busScriptUITesting.js
@@ -3,31 +3,28 @@ const INITIALLNG = 23.78712;
 const WHOLECITYZOOM = 12;
 const LANDMARKZOOM = 14;
 const REFRESHTIMER = 120000;
-const UPDATETIMER = 1500;
+const UPDATETIMER = 1300;
 
-//coords of landmarks
 
 let mapDivElem = document.getElementById("mapholder");
 let curLocBtn = document.getElementById("currentLocationButton");
 let showAllBtn = document.getElementById("showAllButton");
-let clearBtn = document.getElementById("clearButton");
 let buses;
 let busMarkers = [];
 let locationMarkers = [];
 let busLineOptions= [];
 let busUpdateInterval;
 
-//add 1 beginning options for ref
+//add most common options for ref
 busLineOptions[0] = "8";
 let mediapolisLines = ["8", "17"];
 let tamkLines = ["1", "5", "8", "28A", "28B", "28C", "28Y", "29", "80", "90"];
 //might add this later let universityLines = ["2", "6", "9A", "9B", "15", "28B", "40"];
-let allBusLines = ["1", "2", "3A", "3B", "4", "4Y", "5", "6", "8", "9A", "9B", "10", "11", "12", "14", "15", "17", "20", "20R", "21", "24", "25", "26", "28A", "28A", "28B", "28C", "28Y", "29", "31", "32", "33", "33Y", "34", "35", "37", "38", "40", "45", "50A", "50B", "50C", "55", "55K", "65N", "65X", "70", "71", "71K", "71SK", "72", "73", "73K", "74", "79", "80", "80Y", "81", "83", "84", "85", "90", "137"];
-
-curLocBtn.addEventListener('click', showCurrentLocation);
-clearBtn.addEventListener('click', clearAllMarkers);
+let allBusLines = ["1", "1A", "2", "3A", "3B", "4", "4Y", "5", "6", "8", "9A", "9B", "10", "11", "12", "14", "15", "17", "20", "20R", "21", "24", "25", "26", "28A", "28A", "28B", "28C", "28Y", "29", "31", "32", "33", "33Y", "34", "35", "37", "38", "40", "45", "50A", "50B", "50C", "55", "55K", "65N", "65X", "70", "70Y", "71", "71K", "71SK", "72", "73", "73K", "74", "79", "80", "80Y", "81", "83", "84", "85", "85V", "90", "137"];
 
 //event listeners for UI
+document.getElementById("currentLocationButton").addEventListener('click',showCurrentLocation);
+document.getElementById("clearButton").addEventListener('click', clearAllMarkers);
 document.getElementById("showTamk").addEventListener('click', function(){showLandmark("Tamk");});
 document.getElementById("showMediapolis").addEventListener('click', function(){showLandmark("Mediapolis");});
 document.getElementById("showAllMediapolis").addEventListener('click', function(){locationLines("Mediapolis");});
@@ -37,10 +34,13 @@ document.getElementById("showAllTamk").addEventListener('click', function(){loca
 let myMap;
 let geocoder = new google.maps.Geocoder();
 
-let staticData = "http://lissu-api.herokuapp.com/";
+//bus Data
+let busDataAPI = "http://lissu-api.herokuapp.com/";
 
+//initial needed functions
 showGoogleMap();
 initialData();
+//refresh timer in case any new buses start running
 let busArrayRefresh = setInterval(initialData,REFRESHTIMER);
 
 /*display the map fuction*/
@@ -61,7 +61,7 @@ function showGoogleMap() {
 //get the data for the first time to create the array
 function initialData() {
     clearAllMarkers();
-    fetch(staticData)
+    fetch(busDataAPI)
         .then(function(response) {
             if (response.status !== 200) {
                 // data transfer not complete
@@ -75,13 +75,14 @@ function initialData() {
         .catch(function(err) {
             console.log('Fetch Error :' + err);
     });
+    //start update interval
     let busUpdateInterval = setInterval(updateBusData, UPDATETIMER);
 }
 
 //have an interval that gets the data to then update the markers
 function updateBusData() {
     
-    fetch(staticData)
+    fetch(busDataAPI)
         .then(function(response) {
             if (response.status !== 200) {
                 // data transfer not complete
@@ -100,7 +101,7 @@ function updateBusData() {
 //bus data passed to show all markers
 function allBusData() {
     clearAllMarkers();
-    fetch(staticData)
+    fetch(busDataAPI)
         .then(function(response) {
             if (response.status !== 200) {
                 // data transfer not complete
@@ -125,7 +126,7 @@ function initialBusData(thisBus) {
         if (busLineOptions.includes(thisBus.line)) {
             let myMarker = new google.maps.Marker({
                 position: currentBusLocation,
-                icon: "img/busMarker8.png",//"img/busMarker" + thisBus.line + ".png",
+                icon: "img/busMarker" + thisBus.line + ".png",
                 map:myMap,
                 title: thisBus.line + " to " + thisBus.destination,
             });
@@ -136,6 +137,7 @@ function initialBusData(thisBus) {
         } 
 }
 
+//update the currently selected buses
 function busLocationUpdater(thisBus) {
     //console.log("Bus = " + thisBus.id);
     if (busLineOptions.includes(thisBus.line)) {
@@ -143,10 +145,7 @@ function busLocationUpdater(thisBus) {
         //if this exists update location
         for (let n = 0; n < busMarkers.length; n++ ) {
             if (thisBus.id == busMarkers[n].busId) {
-                
-                //console.log(busMarkers[n].busId + " " + thisBus.id);
                 busMarkers[n].busMarker.setPosition(new google.maps.LatLng(thisBus.latitude,thisBus.longitude));
-                
             }
         }
         
@@ -158,7 +157,7 @@ function showAllBuses(thisBus) {
     let currentBusLocation = new google.maps.LatLng(thisBus.latitude,thisBus.longitude);
     let myMarker = new google.maps.Marker({
         position: currentBusLocation,
-        icon: "img/busMarker8.png",
+        icon: "img/busMarker" + thisBus.line + ".png",
         map: myMap,
         title: thisBus.line + " to " + thisBus.destination,
     });
@@ -215,19 +214,18 @@ function lineBtnUpdate(clickedBtn) {
             buttonElems[i].classList.remove("btn-light");
         }
         busLineOptions.push(clickedBtn);
-        
-        //btnElem.classList.remove("btn-light");
-        //btnElem.classList.add("btn-dark");
         initialData();
     }
     console.log(busLineOptions);
 }
 
+//display the bus lines that go to selected location
 function locationLines(location) {
     //update the line buttons and markers and line options
     if (location == "Tamk") {
         busLineOptions = tamkLines;
         console.log(busLineOptions);
+        //update all markers to match selection
         for (let i = 0; i < busLineOptions.length; i++) {
             let buttonElems = document.getElementsByClassName("line" + busLineOptions[i]);
             for (let i = 0; i < buttonElems.length; i++) {
@@ -276,6 +274,7 @@ function locationLines(location) {
     zoomMapOut(locReset);
 }
 
+//make all buses selected and btns updated
 function allBusLineBtnUpdater() {
     busLineOptions = allBusLines;
     for (let i = 0; i < busLineOptions.length; i++) {
@@ -290,8 +289,8 @@ function allBusLineBtnUpdater() {
     zoomMapOut(locReset);
 }
 
+//display the selected landmark based on geolocation
 function showLandmark(landmark) {
-    //add a unique marker for Tamk
     let address = landmark + ", Tampere, Finland";
 	let addressLocation;
 	let foundAddress = false;
@@ -302,7 +301,8 @@ function showLandmark(landmark) {
               addressLocation = results[0].geometry.location;
               foundAddress = true;
 			  let myMarker = new google.maps.Marker({
-							position:addressLocation,
+                            position:addressLocation,
+                            icon: "img/" + landmark + "Marker.png",
 							map:myMap,
 							title:landmark,
               });
@@ -317,12 +317,14 @@ function showLandmark(landmark) {
 	  });
 }
 
+//slight zoom for clear area
 function zoomMapIn(location) {
     myMap.panTo(location);
     myMap.setZoom(LANDMARKZOOM);
     myMap.setCenter(location);
 }
 
+//resets the zoom
 function zoomMapOut(location) {
     myMap.panTo(location);
     myMap.setZoom(WHOLECITYZOOM);
